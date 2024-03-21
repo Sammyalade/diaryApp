49abd95422a15bc88be1c1fdf88fa0b10d50edd2
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-21 20:51:23 +0100

    dairyApp

--- src/africa/semicolon/dairyApp/datas/repositories/DiaryRepository.java
+++ src/africa/semicolon/dairyApp/datas/repositories/DiaryRepository.java
@@ -1,7 +1,7 @@
-package datas.repositories;
+package africa.semicolon.dairyApp.datas.repositories;
 
 
-import datas.models.Diary;
+import africa.semicolon.dairyApp.datas.models.Diary;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface DiaryRepository extends MongoRepository<Diary, String> {
