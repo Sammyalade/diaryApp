5de19694ca720f3e35779c0350e8744fb8a63178
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-21 20:42:34 +0100

    dairyApp

--- src/datas/models/Diary.java
+++ src/datas/models/Diary.java
@@ -1,9 +1,11 @@
 package datas.models;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
+@Data
 public class Diary {
     @Id
     private String username;
