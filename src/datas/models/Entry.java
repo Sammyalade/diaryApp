4aae8f0c7fe98dd08fe254a4591b3de7b551378f
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-21 20:42:47 +0100

    dairyApp

--- src/datas/models/Entry.java
+++ src/datas/models/Entry.java
@@ -1,10 +1,12 @@
 package datas.models;
 
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
+@Data
 public class Entry {
 
     @Id
