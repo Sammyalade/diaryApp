6d41c79e2a8e1a904ac4e6df3f9fe5ec46cadd18
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-22 11:47:59 +0100

    dairyApp

--- src/africa/semicolon/dairyApp/services/DiaryService.java
+++ src/africa/semicolon/dairyApp/services/DiaryService.java
@@ -1,7 +1,24 @@
 package africa.semicolon.dairyApp.services;
 
+import africa.semicolon.dairyApp.datas.models.Diary;
+import africa.semicolon.dairyApp.datas.models.Entry;
+import dtos.EntryCreationRequest;
+import dtos.EntryUpdateRequest;
+import dtos.LoginRequest;
+import dtos.RegisterRequest;
 import org.springframework.stereotype.Service;
 
 @Service
-public class DiaryService {
+public interface DiaryService {
+
+    Diary register(RegisterRequest registerRequest);
+    void login(LoginRequest loginRequest);
+    void logout(String username);
+    Entry createEntry(EntryCreationRequest entryCreationRequest);
+    void updateEntry(EntryUpdateRequest entryUpdateRequest);
+    void getAllEntries(String username);
+    void changePassword(LoginRequest loginRequest);
+    void removeAllDiaries();
+    Entry checkEntryById(String username, long id);
+    void deleteEntry(String username, long id);
 }
