c8397fec63c800011fb4c5c101724a54a93ab02b
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-22 23:03:34 +0100

    dairyApp

--- src/africa/semicolon/dairyApp/services/EntryService.java
+++ src/africa/semicolon/dairyApp/services/EntryService.java
@@ -1,7 +1,27 @@
 package africa.semicolon.dairyApp.services;
 
+import africa.semicolon.dairyApp.datas.models.Entry;
+import dtos.EntryCreationRequest;
+import dtos.EntryUpdateRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class EntryService {
+public interface EntryService {
+
+    Entry create(EntryCreationRequest newEntry);
+
+    List<Entry> getAllEntries(String username);
+
+    Entry checkEntryBy(String username);
+    Optional<Entry> checkEntryBy(long id);
+    void delete(long id);
+
+    void delete(Entry entry);
+
+    Entry update(EntryUpdateRequest entryUpdateRequest);
+
+    void removeAllEntries();
 }
