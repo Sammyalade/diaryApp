69a3cbe0aa600f7da65a3d13048f5a0cc2fdca49
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-22 23:03:49 +0100

    dairyApp

--- src/africa/semicolon/dairyApp/services/EntryServiceImpl.java
+++ src/africa/semicolon/dairyApp/services/EntryServiceImpl.java
@@ -1,4 +1,72 @@
 package africa.semicolon.dairyApp.services;
 
-public class EntryServiceImpl {
+import africa.semicolon.dairyApp.datas.models.Entry;
+import africa.semicolon.dairyApp.datas.repositories.EntryRepository;
+import dtos.EntryCreationRequest;
+import dtos.EntryUpdateRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+@Service
+public class EntryServiceImpl implements EntryService{
+
+    private static EntryRepository entryRepository;
+
+    @Override
+    public Entry create(EntryCreationRequest entryCreationRequest) {
+        Entry newEntry = new Entry();
+        newEntry.setAuthor(entryCreationRequest.getUsername());
+        newEntry.setTitle(entryCreationRequest.getTitle());
+        newEntry.setBody(entryCreationRequest.getBody());
+        return entryRepository.save(newEntry);
+    }
+
+    @Override
+    public List<Entry> getAllEntries(String username) {
+        List<Entry> result = new ArrayList<>();
+        for(Entry entry: entryRepository.findAll()) {
+            if (entry.getAuthor().equals(username.toLowerCase())){
+                result.add(entry);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Entry checkEntryBy(String username) {
+        return entryRepository.findByUsername(username);
+    }
+
+
+    @Override
+    public Optional<Entry> checkEntryBy(long id) {
+        return entryRepository.findById(String.valueOf(id));
+    }
+
+    @Override
+    public void delete(long id) {
+        entryRepository.delete(id);
+    }
+
+    @Override
+    public void delete(Entry entry) {
+        entryRepository.delete(entry);
+    }
+
+    @Override
+    public Entry update(EntryUpdateRequest entryUpdateRequest) {
+        entryRepository.delete(entryRepository.findById(entryUpdateRequest.getId()));
+        Entry newEntry = new Entry();
+        newEntry.setAuthor(entryUpdateRequest.getUsername());
+        newEntry.setTitle(entryUpdateRequest.getTitle());
+        newEntry.setBody(entryUpdateRequest.getBody());
+        return entryRepository.save(newEntry);    }
+
+    @Override
+    public void removeAllEntries() {
+        entryRepository.deleteAll();
+    }
+    }
 }
