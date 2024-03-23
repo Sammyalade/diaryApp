1dbfd3bd89f9bc168c76093e69b138144afcfc31
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-23 19:57:24 +0100

    dairyApp

--- src/africa/semicolon/dairyApp/datas/repositories/EntryRepository.java
+++ src/africa/semicolon/dairyApp/datas/repositories/EntryRepository.java
@@ -4,8 +4,5 @@ import africa.semicolon.dairyApp.datas.models.Entry;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface EntryRepository extends MongoRepository<Entry, String> {
-
-    Entry findByUsername(String username);
-
-    void delete(long id);
+    Entry findByTitle(String title);
 }
