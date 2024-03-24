dba43579ddc15c295f7ad085e1def642d039e2b5
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-24 12:33:53 +0100

    dairyApp

--- src/africa/semicolon/dairyApp/exceptions/UserNotFoundException.java
+++ src/africa/semicolon/dairyApp/exceptions/UserNotFoundException.java
@@ -1,4 +1,4 @@
-package exceptions;
+package africa.semicolon.dairyApp.exceptions;
 
 public class UserNotFoundException extends DiaryAppException{
     public UserNotFoundException(String message) {
