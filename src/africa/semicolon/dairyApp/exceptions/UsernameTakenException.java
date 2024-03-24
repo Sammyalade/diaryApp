f4dda044269f7c40269388a793dba142c1a8aa34
Sammyalade <olufemialadeyelu@gmail.com>
2024-03-24 12:33:41 +0100

    dairyApp

--- src/africa/semicolon/dairyApp/exceptions/UsernameTakenException.java
+++ src/africa/semicolon/dairyApp/exceptions/UsernameTakenException.java
@@ -1,4 +1,4 @@
-package exceptions;
+package africa.semicolon.dairyApp.exceptions;
 
 public class UsernameTakenException extends DiaryAppException{
     public UsernameTakenException(String message) {
